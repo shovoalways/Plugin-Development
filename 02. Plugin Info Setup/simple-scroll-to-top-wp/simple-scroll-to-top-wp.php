e1936e1a6ecf57b4554017bb0b1b9bc50aa9e02e
Ali Hossain <68044724+shovoalways@users.noreply.github.com>
2023-01-29 18:51:42 +0600

    Plugin Info Setup

---
+++ 02. Plugin Info Setup/simple-scroll-to-top-wp/simple-scroll-to-top-wp.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:       Simple Scroll To Top WP
+ * Plugin URI:        https://wordpress.org/plugins/simple-scroll-to-top-wp/
+ * Description:       Simple Scroll to top plugin will help you to enable Back to Top button to your WordPress website.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Ali Hossain
+ * Author URI:        https://alihossain.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://github.com/shovoalways
+ * Text Domain:       sstt
+ */
+
+
+?>
